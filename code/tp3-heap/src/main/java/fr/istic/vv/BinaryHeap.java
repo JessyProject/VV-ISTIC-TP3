8c3b5ca5d39367f4e8456c5d792b07390e809d90
Jessy Grall <jessygrall@outlook.fr>
2022-03-19 02:20:49 +0100

    start binaryHeap

--- code/tp3-heap/src/main/java/fr/istic/vv/BinaryHeap.java
+++ code/tp3-heap/src/main/java/fr/istic/vv/BinaryHeap.java
@@ -1,17 +1,46 @@
 package fr.istic.vv;
 
+
+import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.List;
+import java.util.NoSuchElementException;
 
 class BinaryHeap<T> {
 
-    public BinaryHeap(Comparator<T> comparator) {  }
+    private Comparator<T> comparator;
+    private List<T> elements;
+    private int heapSize;
 
-    public T pop() { return null; }
+    public BinaryHeap(Comparator<T> comparator) {
+        this.comparator = comparator;
+        this.heapSize = 0;
+        this.elements = new ArrayList<>();
+    }
+
+    public T pop() {
+        if(heapSize == 0) throw new NoSuchElementException("Heap is empty");
+        return null;
+    }
 
     public T peek() { return null; }
 
-    public void push(T element) {  }
+    public void push(T element) {
+        elements.add(element);
+        percUp(++heapSize);
+    }
 
-    public int count() { return 0; }
+    public void percUp(int size) {
+        while(size / 2 > 0) {
+            int comp = comparator.compare(elements.get(size), elements.get(size / 2));
+            if(comp < 0) {
+                T tmp = elements.get(size / 2);
+                elements.set(size / 2, elements.get(size));
+                elements.set(size, tmp);
+            }
+        }
+    }
+
+    public int count() { return heapSize; }
 
 }
